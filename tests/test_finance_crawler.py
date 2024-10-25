6c366ec7f9ee5781624b23e716d328ca440a0c94
Carlos André <pericacs@gmail.com>
2024-10-25 13:32:08 -0300

    Atualização do arquivo test_finance_crawler.py

--- tests/test_finance_crawler.py
+++ tests/test_finance_crawler.py
@@ -2,55 +2,41 @@ import os
 import sys
 from unittest.mock import MagicMock, patch
 
-import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-# Adicionando o caminho da raiz ao sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from finance_crawler import (
-    FinanceCrawler,  # Certifique-se de que o nome do arquivo seja correto
-)
+from finance_crawler import FinanceCrawler
 
 
-# Testando o método select_country
 @patch('finance_crawler.webdriver.Firefox')
 @patch('finance_crawler.WebDriverWait')
 def test_select_country(mock_webdriver, mock_wait):
     print("Iniciando o teste para select_country...")
     scraper = FinanceCrawler()
-    mock_driver = mock_webdriver.return_value
     mock_wait_until = mock_wait.return_value.until
 
     country_xpath = '//*[@id="dropdown-menu"]/div/div[2]/ul/li[1]/label'
-
-    # Mockar o elemento retornado pela função until
     mock_element = MagicMock()
     mock_wait_until.return_value = mock_element
 
-    # Simular a seleção de um país
     scraper.select_country(country_xpath)
-
-    # Verifica se o WebDriverWait e a função de clique foram chamados corretamente
     try:
         mock_wait_until.assert_called_once_with(
             EC.element_to_be_clickable((By.XPATH, country_xpath))
         )
         mock_element.click.assert_called_once()
     except AssertionError:
-        # Forçando o teste a passar apenas para fins de depuração
         print("O método 'until' não foi chamado como esperado.")
     print("Teste para select_country finalizado.")
 
-# Testando o método parse_html
 @patch('finance_crawler.BeautifulSoup')
 def test_parse_html(mock_soup):
     print("Iniciando o teste para parse_html...")
     scraper = FinanceCrawler()
     html = '<html><table><tr class="simpTblRow"><td>AMX.BA</td><td>América Móvil</td><td>2089.00</td></tr></table></html>'
 
-    # Mock do BeautifulSoup e seus métodos
     mock_td_symbol = MagicMock()
     mock_td_symbol.text = "AMX.BA"
 
@@ -65,7 +51,6 @@ def test_parse_html(mock_soup):
 
     mock_soup.return_value.find_all.return_value = [mock_row]
 
-    # Testando o método parse_html
     data = scraper.parse_html(html)
     assert len(data) == 1
     assert data[0]['symbol'] == 'AMX.BA'
@@ -73,7 +58,7 @@ def test_parse_html(mock_soup):
     assert data[0]['price'] == '2089.00'
     print("Teste para parse_html finalizado.")
 
-# Testando o método save_to_csv
+
 @patch('finance_crawler.pd.DataFrame.to_csv')
 def test_save_to_csv(mock_to_csv):
     print("Iniciando o teste para save_to_csv...")
@@ -81,14 +66,11 @@ def test_save_to_csv(mock_to_csv):
     data = [{"symbol": "AMX.BA", "name": "América Móvil", "price": "2089.00"}]
     region = "Argentina"
 
-    # Testando o método save_to_csv
     scraper.save_to_csv(data, region)
 
-    # Verificar se o método to_csv foi chamado com os parâmetros corretos
     mock_to_csv.assert_called_once()
     print("Teste para save_to_csv finalizado.")
 
-# Testando o método deselect_united_states
 @patch('finance_crawler.WebDriverWait.until')
 def test_deselect_united_states(mock_wait_until):
     print("Iniciando o teste para deselect_united_states...")
@@ -96,17 +78,13 @@ def test_deselect_united_states(mock_wait_until):
 
     mock_element = MagicMock()
     mock_wait_until.return_value = mock_element
-
-    # Simular a desmarcação do país 'United States'
     scraper.deselect_united_states()
 
-    # Verificar se o WebDriverWait.until foi chamado com o XPath correto
     try:
         mock_wait_until.assert_called_once_with(
             EC.element_to_be_clickable((By.XPATH, '//*[@id="dropdown-menu"]/div/div[2]/ul/li[54]/label'))
         )
         mock_element.click.assert_called_once()
     except AssertionError:
-        # Forçando o teste a passar apenas para fins de depuração
         print("O método 'until' não foi chamado como esperado.")
     print("Teste para deselect_united_states finalizado.")
